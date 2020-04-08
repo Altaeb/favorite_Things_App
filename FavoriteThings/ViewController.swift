a935a60c4d9fdf70816d880c324a6af9c9390650
aftiyah <abd2010ly@gmail.com>
2020-04-08 14:39:28 +0200

    Populating the table with the strings “Do, re, mi, etc.”, continuing our Sound of Music theme.

--- FavoriteThings/ViewController.swift
+++ FavoriteThings/ViewController.swift
@@ -12,40 +12,41 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // MARK: Model
-    // This array with favorite things. Then use this collection to populate your table.
-    let favoriteThings: [String] = [
-        "Raindrops on roses",
-        "Whiskers on kittens",
-        "Bright copper kettles",
-        "Warm woolen mittens"
-    ]
 
-    // MARK: Table View Data Source Methods
-    
-    // number of rows
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return self.favoriteThings.count
-        // TODO: Implement this method to get the correct row count
-  //      let placeholderCount = 2
-    //    return placeholderCount
+        // MARK: Properties
+
+        // Use this string property as your reuse identifier,
+        // Storyboard has been set up for you using this String.
+        let cellReuseIdentifier = "MyCellReuseIdentifier"
+
+        // Choose some data to show in your table
+
+        let model = [
+            ["text" : "Do", "detail" : "A deer. A female deer."],
+            ["text" : "Re", "detail" : "A drop of golden sun."],
+            ["text" : "Mi", "detail" : "A name, I call myself."],
+            ["text" : "Fa", "detail" : "A long long way to run."],
+            ["text" : "So", "detail" : "A needle pulling thread."],
+            ["text" : "La", "detail" : "A note to follow So."],
+            ["text" : "Ti", "detail" : "A drink with jam and bread."]
+        ]
+
+        // MARK: UITableViewDataSource
+
+        // Add the two essential table data source methods here
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return self.model.count;
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell =  tableView.dequeueReusableCell(withIdentifier: self.cellReuseIdentifier)!
+
+            let dictionary = self.model[(indexPath as NSIndexPath).row]
+
+            cell.textLabel?.text = dictionary["text"]
+            cell.detailTextLabel?.text = dictionary["detail"]
+
+            return cell
+        }
     }
-    
-    // cell for row at index path
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteThingCell")!
-        let favoriteThingForRow = self.favoriteThings[(indexPath as NSIndexPath).row]
-        cell.textLabel?.text = favoriteThingForRow
-
-        return cell
-      // TODO: Implement method
-      // 1. Dequeue a reusable cell from the table, using the correct “reuse identifier”
-      // 2. Find the model object that corresponds to that row
-      // 3. Set the images and labels in the cell with the data from the model object
-      // 4. return the cell.
-        
-      //  let placeholderCell = UITableViewCell()
-      //  return placeholderCell
-    }
-}
